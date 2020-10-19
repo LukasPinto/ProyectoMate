6dc8dcbf7c6847bc5f067412488d6a0df33f74fb
lukas <lukas.pinto@alumnos.uv.cl>
2020-10-19 17:54:49 -0300

    se agrego el arhivo hola mundo

---
+++ holamundo.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+int numero;
+char nombre[20];
+cout<<"Ingrese un numero: ";cin>>numero;
+cout<<"ingrese su nombre: ";cin>>nombre;
+cout<<"el numero ingresado es :";cout<<numero<<endl;
+cout <<"el nombre ingreso es: ";cout<<nombre<<endl;
+}
